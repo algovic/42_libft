78256a19fc5e752f07b24804af84fa20fdcb6e3e
algovic <108791644+algovic@users.noreply.github.com>
2022-10-10 14:54:10 +0200

    Add files via upload

--- C/aviso.c
+++ C/aviso.c
@@ -6,8 +6,8 @@ int numero;
           printf("Escriba un número: ");
           scanf("%d", &numero);
           if (numero < 0)
-printf("El número es negativo.\n"); else
-if (numero = 0)
+printf("El número es negativo.\n");
+		  else if (numero = 0)
 printf("El número es cero.\n");
 else
 printf("El número es positivo.\n");
